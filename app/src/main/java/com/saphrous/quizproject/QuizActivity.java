6105122f25f01b4c4e721182c974add295876e13
dadalala <saphrous@gmail.com>
2018-11-05 19:58:41 +0800

    First Commit

---
+++ app/src/main/java/com/saphrous/quizproject/QuizActivity.java
@@ -0,0 +1,45 @@
+package com.saphrous.quizproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private TextView textViewQuestion;
+    private TextView textViewScore;
+    private TextView textViewQuestionCount;
+    private TextView textViewCountDown;
+    private RadioGroup rbGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private Button buttonConfirmNext;
+
+    private List<Question> questionList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        textViewQuestion = findViewById(R.id.textViewQuestion);
+        textViewScore = findViewById(R.id.textViewScore);
+        textViewQuestionCount = findViewById(R.id.textViewQuestionCount);
+        textViewCountDown = findViewById(R.id.textViewCountdown);
+        rbGroup = findViewById(R.id.radioGroup);
+        rb1 = findViewById(R.id.radioButton1);
+        rb2 = findViewById(R.id.radioButton2);
+        rb3 = findViewById(R.id.radioButton3);
+        buttonConfirmNext = findViewById(R.id.buttonConfirmNext);
+
+        QuizDbHelper dbHelper = new QuizDbHelper(this);
+        questionList = dbHelper.getAllQuestions();
+
+    }
+}
